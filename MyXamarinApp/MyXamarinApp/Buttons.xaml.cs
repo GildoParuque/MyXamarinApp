dc683172738f6f0acb59d544d0d882666a7a8ca6
GildoParuque <gildoernestoparuque82@gmail.com>
2022-05-04 16:42:54 +0200

    Add project files.

---
+++ MyXamarinApp/MyXamarinApp/Buttons.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyXamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Buttons : ContentPage
+    {
+        public ICommand ButtonCommand => new Command<string>(CommandButtonClick);
+        public Buttons()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            lblMessage.Text = "Save Sucess";
+        }
+
+        public void CommandButtonClick(string strCommand)
+        {
+            lblMessage.Text = strCommand;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+    }
+}
---
+++ MyXamarinApp/MyXamarinApp/Entries.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyXamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Entries : ContentPage
+    {
+        public Entries()
+        {
+            InitializeComponent();
+            txtName.Text = "Welcome to xamarin";
+        }
+
+        private void Entry_Complete(object sender, EventArgs e)
+        {
+            lblValue.Text = ((Entry)sender).Text;
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lblValue.Text = e.NewTextValue;
+        }
+    }
+}
