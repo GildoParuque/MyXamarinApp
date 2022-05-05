77c252ad1251a1aa9db2d6b89eedf568c0ce4c59
GildoParuque <gildoernestoparuque82@gmail.com>
2022-05-05 15:43:56 +0200

    Check box

---
+++ MyXamarinApp/MyXamarinApp/CheckBoxes.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyXamarinApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CheckBoxes : ContentPage
+    {
+        public CheckBoxes()
+        {
+            InitializeComponent();
+        }
+
+        private void chkXamarin_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if(e.Value == true)
+            {
+                lblMessage.Text = "Yes, Of Course";
+            }
+            else
+            {
+                lblMessage.Text = "No, Never!";
+            }
+        }
+    }
+}
